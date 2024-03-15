240aecae60e51453a71a5b3834ff7be93a6abd3a
Haileamlak Sahle <haileamlaksahle@gmail.com>
2024-03-15 13:24:41 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let myLeads = []
+  
+
+const inputEl = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"))
+
+   if(leadsFromLocalStorage){
+   myLeads = leadsFromLocalStorage
+    renderLeads()
+   }
+
+inputBtn.addEventListener("click", function() {
+
+   myLeads.push(inputEl.value)
+   inputEl.value = '' 
+   localStorage.setItem("myLeads", JSON.stringify(myLeads))
+   renderLeads()
+
+})
+
+ function renderLeads() {
+let listItems = ''
+
+for(let i=0; i<myLeads.length; i++)
+{
+   listItems += `<li><a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`
+
+} 
+ulEl.innerHTML = listItems
+f
+}
+
+
+
